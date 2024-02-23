921a11ed85395f281405c2a6a35964755f9fa34a
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2024-02-23 13:15:00 -0300

    Setup basic project

---
+++ src/test/java/at/technikumwien/menu/MenuApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikumwien.menu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
