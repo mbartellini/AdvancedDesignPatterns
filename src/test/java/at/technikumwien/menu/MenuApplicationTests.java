715265f1fe1744e0d2dcb7887823ef71cfb220b8
Mateo Bartellini Huapalla <mbartellini@itba.edu.ar>
2024-02-25 14:19:08 -0300

    Move test to test class

--- src/test/java/at/technikumwien/menu/MenuApplicationTests.java
+++ src/test/java/at/technikumwien/menu/MenuApplicationTests.java
@@ -3,17 +3,14 @@ package at.technikumwien.menu;
 import at.technikumwien.interfaces.CustomTranslator;
 import at.technikumwien.services.DeeplTranslator;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 @SpringBootTest
 class MenuApplicationTests {
-	@Value("${authKey}")
-	private String authKey;
 
-	private CustomTranslator translator = DeeplTranslator.getInstance(authKey);
+	private final CustomTranslator translator = DeeplTranslator.getInstance();
 
 	@Test
 	void contextLoads() {
